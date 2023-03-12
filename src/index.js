7c846f35bafb3efd6f05d34889dea11d9d731a7e
Sergio Esteban Torres <myplancash@gmail.com>
2023-03-12 09:20:18 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <div className="wrapper">
+      <App />
+    </div>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
